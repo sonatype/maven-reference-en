5a3c65409d9c98fa8cca9725b95d7cb70a9e3ff7
Tim O'Brien <tobrien@discursive.com>
2011-08-27 12:06:24 -0500

    Added a copy statement for public-book.html

--- assemble.sh
+++ assemble.sh
@@ -5,4 +5,5 @@ mkdir -p target/site/pdf
 cp -r target/book-mvnref.chunked/* target/site/reference
 cp target/book-mvnref.pdf target/site/pdf/mvnref-pdf.pdf
 python template.py
+cp target/site/reference/index.html target/site/reference/public-book.html
 rsync -e ssh -av target/site/* deployer@www.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/mvnref-book/
