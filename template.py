6c30dfff564420fe8543ba864517f0465efab958
Tim O'Brien <tobrien@discursive.com>
2011-09-28 17:59:12 -0500

    Fixed some ridiculous typos and added the latex style to render Crown Q.

--- template.py
+++ template.py
@@ -3,12 +3,12 @@ import glob
 import os
 
 t = airspeed.Template(open("site/book-template.html", "r").read())
-bookTitle = "Repository Management with Nexus"
-bookId = "nxbook"
+bookTitle = "Maven: The Complete Reference"
+bookId = "mvnrefbook"
  
 path = 'target/site/reference'
 for infile in glob.glob( os.path.join(path, '*.html') ):
   body = open(infile, "r").read()
   body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
-  title = "Repository Management with Nexus"
+  title = "Maven: The Complete Reference"
   open(infile, "w").write( t.merge(locals()) );
