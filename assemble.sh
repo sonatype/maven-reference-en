f670879e3d1fcce05b50d3aab672a2d591bce4d8
Tim O'Brien <tobrien@discursive.com>
2011-09-29 11:51:08 -0500

    Added logic to ensure that the Book's example are added to the files published to the book web site

--- assemble.sh
+++ assemble.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+pushd .
+cd examples
+mvn clean install
+popd 
+
 mkdir -p target/site/reference
 mkdir -p target/site/pdf
+
+cp examples/target/mvnref-book-examples-1.0-SNAPSHOT-project.zip target/site/mvnref-examples.zip
+
 cp -r target/book-mvnref.chunked/* target/site/reference
 cp target/book-mvnref.pdf target/site/pdf/mvnref-pdf.pdf
 python template.py
