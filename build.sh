43c0abc8838d38b1998ccc9ae6f585da9dc623a1
Manfred Moser <manfred@simpligility.com>
2013-12-02 16:25:27 -0800

    removed dblatex logo display in pdf

--- build.sh
+++ build.sh
@@ -14,11 +14,11 @@ cp -r figs target
 cp -r images target
 cp -r promos/* target
 
-a2x -v -k -fpdf -dbook --dblatex-opts=" -P latex.output.revhistory=0  -s ./latex/custom-docbook.sty" -D target book-mvnref.asciidoc
+a2x -v -k -fpdf -dbook --dblatex-opts=" -P doc.publisher.show=0 -P latex.output.revhistory=0  -s ./latex/custom-docbook.sty" -D target book-mvnref.asciidoc
 
 # Build the Chunked HTML
 a2x -v -k -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl \
     --xsltproc-opts "--stringparam chunk.section.depth 1 --stringparam toc.section.depth 2" \
     -dbook \
-    --dblatex-opts=" -P latex.output.revhistory=0" \
+    --dblatex-opts=" -P doc.publisher.show=0 -P latex.output.revhistory=0" \
     -D target book-mvnref.asciidoc
