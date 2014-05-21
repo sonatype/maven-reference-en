97d2ad8731137edf36171ea90466a55408b92f59
Manfred Moser <manfred@simpligility.com>
2014-05-21 11:14:53 -0700

    adding search input box

--- template.py
+++ template.py
@@ -7,15 +7,17 @@ bookTitle = "Maven: The Complete Reference"
 bookId = "ss-book-mvnref"
 path = 'target/site/reference'
 for infile in glob.glob( os.path.join(path, '*.html') ):
-  print "Reading File: " + infile
-  body = open(infile, "r").read()
-  title = body[ body.index( "<title>" ) + 7 : body.rindex("</title>") ]
-  body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
-
-  if "index.html" in infile:
-    print ("Replacing bookTitle  - replacing with ToC" )
-    title = "Table of Contents"
-    body = body.replace(bookTitle, title)
-    
-  open(infile, "w").write( t.merge(locals()) );
+    if infile.endswith( 'search.html'):
+    print( "  Ignoring search.html" )
+  else:
+    print "Reading File: " + infile
+    body = open(infile, "r").read()
+    title = body[ body.index( "<title>" ) + 7 : body.rindex("</title>") ]
+    body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
   
+    if "index.html" in infile:
+      print ("Replacing bookTitle  - replacing with ToC" )
+      title = "Table of Contents"
+      body = body.replace(bookTitle, title)
+      
+    open(infile, "w").write( t.merge(locals()) );
