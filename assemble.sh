94a779626014ebb82b1ca743068a4a6d53227beb
Manfred Moser <manfred@simpligility.com>
2015-05-14 10:52:35 -0700

    invoking external tempalting

--- assemble.sh
+++ assemble.sh
@@ -11,11 +11,15 @@ mkdir -p target/site/pdf
 cp examples/target/mvnref-book-examples-1.0-SNAPSHOT-project.zip target/site/mvnref-examples.zip
 
 cp -r target/book-mvnref.chunked/* target/site/reference
-
-laf=../documentation-wrapper
-cp -r $laf/* target/site/reference
-
 cp target/book-mvnref.pdf target/site/pdf/mvnref-pdf.pdf
 
-python template.py -l=$laf/template.html
-cp target/site/reference/index.html target/site/reference/public-book.html
+echo "Invoking templating process"
+../documentation-wrapper/apply-template.sh ../maven-reference-en/target/site/reference "Maven: The Complete Reference" "017156762307045728421:e5xgzsj0rjw"
+
+
+#laf=../documentation-wrapper
+#cp -r $laf/* target/site/reference
+
+
+#python template.py -l=$laf/template.html
+#cp target/site/reference/index.html target/site/reference/public-book.html
